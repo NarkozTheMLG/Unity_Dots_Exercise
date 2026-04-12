55c792cb1db4007db2439e4eea598755155ded92
Ömer Keskin <omerkeskin980@gmail.com>
2026-04-12 23:30:22 +0300

    Physics with DOTS
    RefRO & RefRW

---
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -0,0 +1,33 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using UnityEngine;
+using Unity.Physics;
+partial struct UnitMoverSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state){
+     foreach((
+            RefRW<LocalTransform> localTransform,
+            RefRO<MoveSpeed> moveSpeed,
+            RefRW<PhysicsVelocity> physicsVelocity) 
+            in SystemAPI.Query
+                <RefRW<LocalTransform>,
+                RefRO<MoveSpeed>,
+                RefRW<PhysicsVelocity>>())
+        {
+            float3 targetPosition = localTransform.ValueRO.Position+ new float3(10,0,0); 
+            float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
+            moveDirection = math.normalize(moveDirection);
+
+            localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());
+
+            physicsVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.value;
+            physicsVelocity.ValueRW.Angular = float3.zero;
+            //localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.value * SystemAPI.Time.DeltaTime;
+        }
+    }
+
+  
+}
